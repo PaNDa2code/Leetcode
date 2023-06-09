5472c6019dd863418af883df0833efbd9703ccdf
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-09 15:49:03 +0300

    Time: 137 ms (40.24%), Space: 16.7 MB (21.82%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -1,14 +1,39 @@
 class Solution:
     def nextGreatestLetter(self, letters: list[str], target: str) -> str:
 
-        let = []
-        Tar = ord(target)
+        # let = []
+        # Tar = ord(target)
         
-        for litter in letters:
-            if ord(litter) > Tar:
-                let.append(ord(litter))
+        # for litter in letters:
+        #     if ord(litter) > Tar:
+        #         let.append(ord(litter))
         
-        if let == []:
-            return letters[0]
+        # if let == []:
+        #     return letters[0]
 
-        return chr(min(let))
+        # return chr(min(let))
+
+        # binary search
+        
+
+        right = len(letters) - 1
+        left = 0
+        t = ord(target)
+        
+        while left <= right:
+
+            mid = (right + left) // 2
+        
+            if ord(letters[mid]) <= t:
+
+                left = mid + 1
+
+            else:
+
+                right = mid - 1
+        
+
+        if left ==len(letters):
+            return letters[0]
+        else :
+            return letters[left]
