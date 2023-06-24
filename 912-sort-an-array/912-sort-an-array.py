b9212d7fddc7621a55a24408fc13117cf52dd791
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-24 21:11:10 +0300

    Time: 1819 ms (72.49%), Space: 26.1 MB (23.49%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -27,7 +27,24 @@ def merge_sort(nums):
     
     return merge(left_sorted,right_sorted)
 
+def counting_sort(nums):
+	
+	count = {}
+	
+	min_val , max_val = min(nums) , max(nums)
+	
+	for i in nums:
+		count[i] = count.get(i,0) + 1
+	x = 0
+	for i in range(min_val,max_val+1):
+		while count[i] > 0:
+			nums[x] = i
+			x += 1
+			count[i] -= 1
+                        
+	return nums
 
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        return merge_sort(nums)
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if max(nums) - min(nums) <= 2: return counting_sort(nums)
+        return merge_sort(nums)	
