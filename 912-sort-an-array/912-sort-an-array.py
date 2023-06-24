ba1fc4decf2a1a8f3c0f75e72c54994ceb6795ae
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-24 20:51:39 +0300

    Time: 2027 ms (61.77%), Space: 26.3 MB (22.39%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,18 +1,33 @@
+def merge(nums1,nums2):
+    
+    out = []
+    x,y = 0,0
+    
+    while x < len(nums1) and y < len(nums2):
+        if nums1[x] <= nums2[y]:
+            out.append(nums1[x])
+            x += 1
+        else:
+            out.append(nums2[y])
+            y += 1
+            
+    return out + nums1[x:] + nums2[y:]
+
+def merge_sort(nums):
+    
+    if len(nums)<= 1:
+        
+        return nums
+    
+    mid = len(nums) // 2
+    
+    left , right = nums[:mid] , nums[mid:]
+    
+    left_sorted , right_sorted = merge_sort(left) , merge_sort(right)
+    
+    return merge(left_sorted,right_sorted)
+
+
 class Solution:
-	def sortArray(self, nums: list[int]) -> list[int]:
-		
-		hashmap = {}
-		min_val , max_val = min(nums) , max(nums)
-		# counting hashmap
-		for i in nums:
-			hashmap[i] = hashmap.get(i,0) + 1
-			
-		x = 0
-		for i in range(min_val,max_val+1):
-			
-			while hashmap.get(i,0)>0:
-				nums[x] = i
-				x += 1
-				hashmap[i] -= 1
-				
-		return nums
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return merge_sort(nums)
