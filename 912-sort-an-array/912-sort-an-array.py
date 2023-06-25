cab31aa87bff19e6929ef2a2d896c2e488adb770
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-25 13:02:10 +0300

    Time: 906 ms (80.47%), Space: 30 MB (19.10%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.py
@@ -0,0 +1,51 @@
+def merge(nums1,nums2):
+    
+    out = []
+    x,y = 0,0
+    
+    while x < len(nums1) and y < len(nums2):
+        if nums1[x] <= nums2[y]:
+            out.append(nums1[x])
+            x += 1
+        else:
+            out.append(nums2[y])
+            y += 1
+            
+    return out + nums1[x:] + nums2[y:]
+
+def merge_sort(nums):
+    
+    if len(nums)<= 1:
+        
+        return nums
+    
+    mid = len(nums) // 2
+    
+    left , right = nums[:mid] , nums[mid:]
+    
+    left_sorted , right_sorted = merge_sort(left) , merge_sort(right)
+    
+    return merge(left_sorted,right_sorted)
+
+def counting_sort(nums):
+	
+	count = {}
+	
+	min_val , max_val = min(nums) , max(nums)
+	
+	for i in nums:
+		count[i] = count.get(i,0) + 1
+    
+	x = 0
+	for i in range(min_val,max_val+1):
+		while count.get(i,0) > 0:
+			nums[x] = i
+			x += 1
+			count[i] -= 1
+                        
+	return nums
+
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if (max(nums) - min(nums)) / len(nums) <= 2: return counting_sort(nums)
+        return merge_sort(nums)	
