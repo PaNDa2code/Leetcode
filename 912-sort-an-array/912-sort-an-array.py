e459ffe2b2f592f61ae6081303b75c8b6121f725
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-25 13:00:29 +0300

    Update 912-sort-an-array.py

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -38,9 +38,10 @@ def counting_sort(nums):
 	
 	for i in nums:
 		count[i] = count.get(i,0) + 1
+    
 	x = 0
 	for i in range(min_val,max_val+1):
-		while count[i] > 0:
+		while count.get(i,0) > 0:
 			nums[x] = i
 			x += 1
 			count[i] -= 1
@@ -49,5 +50,5 @@ def counting_sort(nums):
 
 class Solution:
     def sortArray(self, nums: list[int]) -> list[int]:
-        if max(nums) - min(nums) <= 2: return counting_sort(nums)
+        if (max(nums) - min(nums)) / len(nums) <= 2: return counting_sort(nums)
         return merge_sort(nums)	
