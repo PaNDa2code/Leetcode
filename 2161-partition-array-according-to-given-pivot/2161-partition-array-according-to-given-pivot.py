84ff754c04d4aa48428296893cef00e9d3a3322c
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-26 13:53:21 +0300

    Time: 1385 ms (60.29%), Space: 34 MB (16.01%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        return [i for i in nums if i < pivot] + [i for i in nums if i == pivot] + [i for i in nums if i > pivot]
+        
+        
+        
+        
