8bd6da7044410c19949e89069af1c0d3f25135d5
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-29 16:11:30 +0000

    Time: 85 ms (89.12%), Space: 16.6 MB (96.35%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for num in nums:
+            if len(sub) == 0 or sub[-1] < num:
+                sub.append(num)
+            else:
+                idx = bisect.bisect_left(sub,num)
+                sub[idx] = num
+        return len(sub)
