6cbbb0c2553c473170924dab6880c156d534bfc3
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-06-30 14:47:16 +0000

    Time: 92 ms (82.83%), Space: 16.7 MB (35.45%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -1,10 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         sub = []
+        # O(n)
         for num in nums:
+
             if len(sub) == 0 or sub[-1] < num:
                 sub.append(num)
             else:
-                idx = bisect.bisect_left(sub,num)
+
+                # binary search
+                # O(log n)
+
+                left = 0
+                right = len(sub)-1
+
+                while left <= right:
+                    mid = (left+right)//2
+                    if sub[mid] < num:
+                        left = mid +1
+                    else:
+                        right = mid -1
+
+
+                idx = left
                 sub[idx] = num
+                
         return len(sub)
