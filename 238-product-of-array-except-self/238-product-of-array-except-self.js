6665c87611abb0421dd6eb1bf0626c8d5f067073
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-03 14:13:17 +0000

    Time: 101 ms (78.93%), Space: 55 MB (42.86%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -2,36 +2,58 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var productExceptSelf = function(nums) {
+    
+//     let sum = 1
+//     let tmp = []
+//     let zeros = []
+    
+//     for(let i in nums){
+//         if (nums[i] == 0){
+//             zeros.push(i)
+//         }else{
+//             sum *= nums[i]
+//         }
+//     };
+    
+//     if(zeros.length > 1 || zeros.length == nums.length){
+//         tmp = Array(nums.length).fill(0)
+//         return tmp
+//     }else if(zeros.length == 1){
+//         tmp = Array(nums.length).fill(0)
+//         tmp[zeros[0]] = sum
+//         return tmp
+//     }else{
+//         for(let num of nums){
+//             tmp.push(sum/num)
+            
+//         };
+//         return tmp
+//     }
+    
+    
+    
+    
+// };
+
 var productExceptSelf = function(nums) {
     
-    let sum = 1
-    let tmp = []
-    let zeros = []
+    const n = nums.length;
+    let output = new Array(n).fill(1);
     
-    for(let i in nums){
-        if (nums[i] == 0){
-            zeros.push(i)
-        }else{
-            sum *= nums[i]
-        }
+    var prefix_product = 1;
+    
+    for(let i = 0;i < n;i++){
+        output[i] *= prefix_product;
+        prefix_product *= nums[i]
     };
     
-    if(zeros.length > 1 || zeros.length == nums.length){
-        tmp = Array(nums.length).fill(0)
-        return tmp
-    }else if(zeros.length == 1){
-        tmp = Array(nums.length).fill(0)
-        tmp[zeros[0]] = sum
-        return tmp
-    }else{
-        for(let num of nums){
-            tmp.push(sum/num)
-            
-        };
-        return tmp
-    }
+    var suffix_product = 1;
     
+    for(let i = n-1; i >= 0; i--){
+        output[i] *= suffix_product ;
+        suffix_product *= nums[i]
+    };
     
-    
-    
-};
+    return output
+}
