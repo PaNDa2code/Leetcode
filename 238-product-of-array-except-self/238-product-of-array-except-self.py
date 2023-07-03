539f4664a586fab4cacf8e44d4925eba2bf61b7c
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-03 13:47:54 +0000

    Time: 221 ms (97.18%), Space: 24 MB (43.19%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,56 +1,48 @@
-import numpy as np
+# import numpy as np
 
 # these is the classic slotion
 # Runtime: 120 ms, faster than 28.92% 
 # Memory Usage: 57.3 MB, less than 9.78%
 
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output = [1] * n
         
-#         tmp,zeros,sum = [],[],1
+        # Compute prefix products
+        prefix_product = 1
+        for i in range(n):
+            output[i] *= prefix_product
+            prefix_product *= nums[i]
         
-#         for i in range(len(nums)):
-#             if nums[i] == 0:
-#                 zeros.append(i)
-#             else:
-#                 sum *= nums[i]
+        # Compute suffix products
+        suffix_product = 1
+        for i in range(n-1, -1, -1):
+            output[i] *= suffix_product
+            suffix_product *= nums[i]
         
-#         if len(zeros) > 1 or len(zeros) == len(nums):
-#             return [0]*len(nums)
-        
-#         elif len(zeros) == 1:           
-            
-#             tmp = [0]*len(nums)
-            
-#             tmp[zeros[0]] = sum
-            
-#             return tmp
-#         else:
-            
-#             for i in range(len(nums)):
-#                 tmp.append(sum//nums[i])
-                
-#             return tmp
+        return output
+
 
 # these is just for testing
 
-class Solution:
-    def productExceptSelf(self, nums):
-        nums = np.array(nums)
-        n = len(nums)
-        zeros = np.where(nums == 0)[0]
+# class Solution:
+#     def productExceptSelf(self, nums):
+#         nums = np.array(nums)
+#         n = len(nums)
+#         zeros = np.where(nums == 0)[0]
 
-        if len(zeros) > 1 or len(zeros) == n:
-            return [0] * n
+#         if len(zeros) > 1 or len(zeros) == n:
+#             return [0] * n
 
-        elif len(zeros) == 1:
-            output = np.zeros(n, dtype=int)
-            product = np.prod(nums[np.nonzero(nums)], dtype=int)
-            output[zeros[0]] = product
-            return output.tolist()
+#         elif len(zeros) == 1:
+#             output = np.zeros(n, dtype=int)
+#             product = np.prod(nums[np.nonzero(nums)], dtype=int)
+#             output[zeros[0]] = product
+#             return output.tolist()
 
-        else:
-            product = np.prod(nums, dtype=int)
-            output = product // nums
-            return output.tolist()
+#         else:
+#             product = np.prod(nums, dtype=int)
+#             output = product // nums
+#             return output.tolist()
 
