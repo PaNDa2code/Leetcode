1a1f02d3fcc5ef54f381f7e459603a0c037cb4fe
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-04 15:05:47 +0000

    Time: 140 ms (79.48%), Space: 18.9 MB (94.67%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,8 +1,15 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        out = 0
+        n = 0 
+        
+        # n ^ 0 = n
+        # n ^ n = 0
+        
+        # so if num is not showed before __ n = num
+        
+        # if n is showed before __ n = 0
         
         for num in nums:
-            out ^= num
+            n ^= num
        
-        return out
+        return n
