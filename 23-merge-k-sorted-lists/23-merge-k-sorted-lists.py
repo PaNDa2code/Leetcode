41405ae555ce5cc2354966c3ae719713df71a6a8
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-10 22:03:02 +0000

    Time: 4421 ms (8.67%), Space: 19.7 MB (99.44%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -0,0 +1,29 @@
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        out_head = ListNode()
+        out_node = out_head
+        k = len(lists)
+        
+        while any(lists):
+            
+            min_node = ListNode(float('inf'))
+            idx = -1
+            
+            for i, node in enumerate(lists):
+                if node and node.val < min_node.val:
+                    min_node = node
+                    idx = i
+            
+            if idx == -1:
+                break
+            
+            out_node.next = min_node
+            out_node = out_node.next
+            
+            lists[idx] = lists[idx].next
+            
+            if not lists[idx]:
+                del lists[idx]
+            
+        return out_head.next
