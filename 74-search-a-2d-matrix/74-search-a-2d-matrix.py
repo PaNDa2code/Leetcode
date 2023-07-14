0a9abd1f98bb9de203c35351a15bc8020fcf3876
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-14 21:06:20 +0000

    Time: 66 ms (21.13%), Space: 16.9 MB (27.80%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,35 +1,60 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        for m in range(len(matrix)):
+     
+        def bs_in_row(row,target):
             
-            if matrix[m][-1] == target:
-                
-                return True
-            
-            elif matrix[m][-1] > target:
-                
                 left = 0
                 
-                right = len(matrix[m]) - 1
+                right = len(matrix[row]) - 1
                 
                 while left <= right:
                     
                     mid = (left+right) // 2
                     
-                    if matrix[m][mid] == target:
+                    if matrix[row][mid] == target:
                         
                         return True
                     
-                    elif matrix[m][mid] > target:
+                    elif matrix[row][mid] > target:
                         
                         right = mid - 1
                         
                     else:
                         
                         left = mid + 1
-                    
-                    
+                
+                return False
             
             
-        return False
+        left = 0
+        
+        right = len(matrix) - 1
+        
+        row = -1
+        
+        while left <= right:
+            
+            mid = (left+right) // 2
+            
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                
+                row = mid
+                
+                break
+            
+            elif matrix[mid][0] > target:
+                
+                right = mid - 1
+                
+            else:
+                
+                left = mid + 1
+                
+        return bs_in_row(row,target)
+        
+    
+                
+
+                    
+                    
+        
