d9844c87873d8e311859743d673b08e0aa22b039
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-16 13:47:51 +0000

    Time: 1678 ms (9.72%), Space: 38.1 MB (84.87%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -1,21 +1,3 @@
-def insert(arr,value):
-    
-    
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if arr[mid] <= value:
-            left = mid + 1
-        elif arr[mid] > value:
-            right = mid - 1
-    
-    arr.insert(left, value)
-            
-        
-    
 class MedianFinder:
 
     def __init__(self):
