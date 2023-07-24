3bb93f5124d050821aeb9a2b54c2b41f261ae5a9
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-24 10:37:25 +0000

    Time: 53 ms (18.45%), Space: 16.3 MB (78.07%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,4 +1,26 @@
 class Solution:
+    
+    
+    
     def myPow(self, x: float, n: int) -> float:
         
-        return x ** n
+        if n == 0:
+            return 1
+        
+        if n < 0:
+            return 1 / self.myPow(x,-n)
+        
+        isOdd = lambda x: x % 2 != 0
+        
+        if isOdd(n):
+            
+            return x * self.myPow(x*x, (n-1) // 2)
+        
+        else:
+            
+            return self.myPow(x*x, n // 2)
+        
+        
+        
+        
+        
