386812c174bac4b90b8a8f6775045a8be30f520b
I am batman <126345041+PaNDa2code@users.noreply.github.com>
2023-07-29 15:16:27 +0000

    Time: 70 ms (98.95%), Space: 16.7 MB (57.51%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,13 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
         stack = []
-
         for token in tokens:
             if token.isdigit() or (token[0] == "-" and token[1:].isdigit()):
                 stack.append(int(token))
             else:
                 second_elm = stack.pop()
-                first_elm = stack.pop()
-                stack.append(int(eval(f'{first_elm}{token}{second_elm}')))
+                first_elm = stack.pop()              
+                match token:
+                    case '+':
+                        stack.append(first_elm+second_elm)
+                    case '-':
+                        stack.append(first_elm-second_elm)
+                    case '*':
+                        stack.append(first_elm*second_elm)
+                    case '/':
+                        stack.append(int(first_elm/second_elm))
 
         return stack[0]
